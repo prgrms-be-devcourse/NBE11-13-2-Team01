496e880dd7556f219d47f806ef40ec81b4d9ca43
ijeong-gyun <jeonggyuni86@gmail.com>
2026-08-12 10:02:30 +0900

    feat/stop 상태 추가

--- src/main/java/com/example/delivery_project/domain/entity/enums/RiskFactorType.java
+++ src/main/java/com/example/delivery_project/domain/entity/enums/RiskFactorType.java
@@ -1,15 +1,15 @@
 package com.example.delivery_project.domain.entity.enums;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum RiskFactorType {
-    HEAVY_RAIN("폭우"),
-    HEAT_WAVE("폭염"),
-    WEATHER_WARNING("기상 특보");
+    HEAVY_RAIN("폭우", 30),
+    HEAT_WAVE("폭염", 20),
+    WEATHER_WARNING("기상 특보", 40);
 
     private final String description;
-    RiskFactorType(String description) {
-        this.description = description;
-    }
+    private final int riskScore;
 }
