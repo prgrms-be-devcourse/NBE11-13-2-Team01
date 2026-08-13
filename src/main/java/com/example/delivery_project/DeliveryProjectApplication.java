aa0c485ada0eba3e01675cee8743176ae67bf271
“HanChaehui” <hch8344@naver.com>
2026-08-13 10:30:50 +0900

    feat: Security 인증 사용자 조회 기반 구현

--- src/main/java/com/example/delivery_project/DeliveryProjectApplication.java
+++ src/main/java/com/example/delivery_project/DeliveryProjectApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = DataSourceAutoConfiguration.class
+)
 public class DeliveryProjectApplication {
 
     public static void main(String[] args) {
